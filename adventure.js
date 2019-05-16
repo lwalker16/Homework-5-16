a7f1bd3cf85d11eeac305ce2e49ea3812288d822
lwalker16 <50379912+lwalker16@users.noreply.github.com>
2019-05-16 15:05:19 -0500

    Add files via upload

---
+++ adventure.js
@@ -0,0 +1,22 @@
+var hero = 'High Elf';
+var villain = 'Witch';
+
+if(hero === 'High Elf') { console.log('You are the Savior of Skyrim' + hero); }
+else if (villain === 'Witch') { console.log('Hello, Evil ' + villain); }
+else { console.log('Peasant');
+
+}
+
+function SkyrimShout() {
+    console.log('Fus Ro Dah');
+}
+
+SkyrimShout();
+
+function lastname(firstname) {
+    var fullname = firstname + ' Dovahkin';
+    console.log(fullname);
+}
+
+lastname('Holy');
+
